34bd7a197cded80ff521ec450dedfba6dd4c923c
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-07-13 15:04:43 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,25 @@
+function add(a, b){
+  return a+=b;
+};
+add(2, 5);
+
+function subtract(a, b){
+  return a-=b;
+};
+subtract(7, 5);
+
+function multiply(a, b){
+  return a*=b;
+};
+multiply(2, 5);
+
+function divide(a, b){
+  return a/=b;
+};
+divide(2, 5);
+
+function inc(n){
+  var n = 166;
+  n++;
+  return n;
+}
